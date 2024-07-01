9c8fef9d4db5a81545e553df78ab0875d47bafce
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-06-30 21:29:19 -0700

    Inheritance notes.py

--- notes.py
+++ notes.py
@@ -1,6 +1,5 @@
 ## Inheritance ##
 
-
 class Animal:  # Base class (parent class)
     def __init__(self):
         pass  # Placeholder for initialization in the Animal class
@@ -10,7 +9,7 @@ class Fish(Animal):  # Inherits from Animal class (child class)
         super().__init__()  # Call the parent class's __init__ method
         pass  # Placeholder for Fish-specific initialization
 
-
+############################################################
 # # Example a) without inheritance
 # class Animal:
 #     def __init__(self):
@@ -25,27 +24,52 @@ class Fish(Animal):  # Inherits from Animal class (child class)
 # nemo = Fish()
 # nemo.swim()
 
-## Example a) with inheritance
-class Animal:
-    def __init__(self):
-        self.num_eyes = 2
-    def breath(self):
-        print("Inhale, exhale.")
+# ## Example a) with inheritance
+# class Animal:
+#     def __init__(self):
+#         self.num_eyes = 2
+#     def breath(self):
+#         print("Inhale, exhale.")
+#
+#
+# class Fish(Animal):
+#     def __init__(self):
+#         super().__init__()
+#     def breath(self):
+#         super().breath()
+#         print("Doing this underwater")
+#     def swim(self):
+#         print("Moving in water.")
+#
+# nemo = Fish()
+# # nemo.swim()
+# nemo.breath()
+# # print(nemo.num_eyes)
+###############################################################
 
-
-class Fish(Animal):
-    def __init__(self):
-        super().__init__()
-    def breath(self):
-        super().breath()
-        print("Doing this underwater")
-    def swim(self):
-        print("Moving in water.")
-
-nemo = Fish()
-# nemo.swim()
-nemo.breath()
-# print(nemo.num_eyes)
+###############################################################
+## Another example
+# class Dog:
+#     def __init__(self):
+#         self.temperament = "loyal"
+#
+#     def bark(self):
+#         print("Woof, woof!")
+#
+#
+# class Labrador(Dog):
+#     def __init__(self):
+#         super().__init__()
+#         self.is_a_good_boy = True
+#
+#     def bark(self):
+#         super().bark()
+#         print("Greetings, good sir. How do you do?")
+#
+#
+# sparky = Labrador()
+# sparky.bark()
+###############################################################
 
 
 
